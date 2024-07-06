f1b3f98e386ec4c915c1ad3176be6758b12fa812
emacs45 <emec.acmi@gmail.com>
2024-07-06 19:44:05 +0200

    clean up repository with .gitignore

--- lib/python3.10/site-packages/pysmi/borrower/__init__.py
+++
@@ -1,8 +0,0 @@
-#
-# This file is part of pysmi software.
-#
-# Copyright (c) 2015-2019, Ilya Etingof <etingof@gmail.com>
-# License: http://snmplabs.com/pysmi/license.html
-#
-from pysmi.borrower.pyfile import PyFileBorrower
-from pysmi.borrower.anyfile import AnyFileBorrower
--- lib/python3.10/site-packages/pysmi/writer/__init__.py
+++
@@ -1,9 +0,0 @@
-#
-# This file is part of pysmi software.
-#
-# Copyright (c) 2015-2019, Ilya Etingof <etingof@gmail.com>
-# License: http://snmplabs.com/pysmi/license.html
-#
-from pysmi.writer.localfile import FileWriter
-from pysmi.writer.pyfile import PyFileWriter
-from pysmi.writer.callback import CallbackWriter
