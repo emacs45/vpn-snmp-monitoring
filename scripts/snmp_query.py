f70d7d26868125b5b1ddf017df4d3210a0feacf8
emacs45 <emec.acmi@gmail.com>
2024-07-14 18:43:46 +0200

    Fix flake8 issues: remove wildcard import and split long lines

--- scripts/snmp_query.py
+++ scripts/snmp_query.py
@@ -1,8 +1,8 @@
 import sqlite3
 from datetime import datetime
 from pysnmp.hlapi import (
-    getCmd, nextCmd, SnmpEngine, CommunityData,
-    UdpTransportTarget, ContextData, ObjectType, ObjectIdentity
+    SnmpEngine, CommunityData, UdpTransportTarget, ContextData,
+    ObjectType, ObjectIdentity, getCmd, nextCmd
 )
 
 
@@ -25,34 +25,29 @@ def insert_data(timestamp, oid, value):
 
 
 def query_snmp_data():
+    # SNMP GET Request
     iterator = getCmd(
         SnmpEngine(),
         CommunityData('public', mpModel=0),
         UdpTransportTarget(('localhost', 161)),
         ContextData(),
         ObjectType(ObjectIdentity('1.3.6.1.2.1.1.1.0'))
-        
     )
 
     error_indication, error_status, error_index, var_binds = next(iterator)
     if error_indication:
         print(error_indication)
     elif error_status:
-        print(
-            '%s at %s' % (
-                error_status.prettyPrint(),
-                error_index and var_binds[int(error_index) - 1][0] or '?'
-            )
-        )
+        print('%s at %s' % (error_status.prettyPrint(),
+                            error_index and var_binds[int(error_index) - 1][0] or '?'))
     else:
         for var_bind in var_binds:
             timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
             oid = var_bind[0].prettyPrint()
             value = var_bind[1].prettyPrint()
             insert_data(timestamp, oid, value)
-    
+
     # SNMP WALK Request
-    
     iterator = nextCmd(
         SnmpEngine(),
         CommunityData('public', mpModel=0),
@@ -80,3 +75,4 @@ def query_snmp_data():
 if __name__ == "__main__":
     create_database()
     query_snmp_data()
+
